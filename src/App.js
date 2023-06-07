918b74d75adb2650db399775fa4071cf7bf51b06
MahFelix <marcelo12543@hotmail.com>
2023-06-06 23:56:20 -0300

    CodeBurger-Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Burger from './assets/burgericon.svg'
+
+import {Container,ContainerItens,Image, H1, Input, Button, InputLabel} from './styles'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <Container>
+     <ContainerItens>
+        <Image alt='Logo-Imagem ' src={Burger} />
+
+          <H1>Faça seu pedido</H1>
+
+              <InputLabel> Pedido </InputLabel>
+        <Input placeholder=' 1 Coca-Cola, 1-X Salada'></Input>
+
+              <InputLabel> Nome do cliente </InputLabel>
+          <Input placeholder='Marta'></Input>
+
+          <Button>
+                Novo pedido
+          </Button>
+
+      </ContainerItens>
+
+    </Container>
+
+);
+
+
+
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,118 @@
+import styled from 'styled-components';
+
+export const ContainerItens = styled.div `
+
+
+ background: #0A0A10;
+
+width: 414px;
+height: 896px;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+justify-content: space-between;
+
+
+`
+
+export const Container = styled.div `
+
+width: 100vw;
+height: 100vh;
+display: flex;
+align-items: center;
+justify-content: center;
+
+
+`
+
+
+
+export const H1 = styled.h1 `
+
+width: 213px;
+height: 33px;
+left: 101px;
+top: 390px;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 700;
+font-size: 28px;
+line-height: 33px;
+
+text-align: center;
+
+color: #FFFFFF;
+
+`
+
+export const Input = styled.input `
+
+width: 370px;
+height: 58px;
+border: none;
+background: rgba(255, 255, 255, 0.25);
+border-radius: 14px;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 300;
+font-size: 18px;
+line-height: 21px;
+color: #FFFFFF;
+
+`
+
+export const InputLabel = styled.p`
+width: 150px;
+height: 1px;
+left: 40px;
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 700;
+font-size: 18px;
+line-height: 55px;
+display: flex;
+padding: 0;
+margin-right: 195px;
+
+
+
+color: #EEEEEE;
+`
+
+
+export const Image = styled.img `
+width: 342px;
+height: 354px;
+left: 36px;
+top: 11px;
+margin-top: 11px;
+
+`
+
+export const Button = styled.button `
+
+width: 342px;
+height: 68px;
+margin-bottom: 75px;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 900;
+font-size: 17px;
+line-height: 2px;
+/* or 15% */
+
+display: flex;
+justify-content: center;
+align-items: center;
+text-align: center;
+
+color: #FFFFFF;
+
+background: #D93856;
+
+`
